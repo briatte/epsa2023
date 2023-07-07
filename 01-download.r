95052ad7346277726ca9b0e735bf4d2020ebf776
François Briatte <briatte@gmail.com>
2023-07-07 12:01:44 +0200

    init

---
+++ 01-download.r
@@ -0,0 +1,98 @@
+library(RSelenium)
+library(rvest)
+library(tidyverse)
+
+fs::dir_create("html")
+fs::dir_create("html/abstracts")
+fs::dir_create("html/sessions")
+
+# all session ids (main programme page manually downloaded)
+p <- read_html("html/Program • EPSA 2023.html") %>%
+  html_nodes("a") %>%
+  html_attr("href") %>%
+  str_subset("/session/\\d{5}") %>%
+  str_replace(".*?#/", "https://virtual.oxfordabstracts.com/#/") %>%
+  unique()
+
+# initialize headless browser ---------------------------------------------
+
+dr <- rsDriver(browser = "firefox",
+               port = 4554L,
+               verbose = FALSE,
+               chromever = NULL)
+
+rd <- dr[["client"]]
+
+# display random page
+i <- sample(p, 1)
+rd$navigate(i)
+Sys.sleep(3)
+
+# click time zone menu list
+tz1 <- rd$findElement(using = "css selector", "button.flex")
+tz1$clickElement()
+
+# click first item (GMT+1)
+tz2 <- rd$findElement(using = "css selector", "button.text-left")
+tz2$clickElement()
+
+# click 'Save'
+tz3 <- rd$findElement(using = "css selector", "button.inline-flex")
+tz3$clickElement()
+
+# download sessions and abstracts -----------------------------------------
+
+for (i in sample(p)) {
+
+  f <- str_c("html/sessions/session_", str_extract(i, "\\d+$"), ".html")
+  if (!fs::file_exists(f)) {
+
+    rd$navigate(i)
+    Sys.sleep(2)
+
+    rd$getPageSource()[[1]] %>%
+      readr::write_lines(f)
+
+  }
+
+  h <- read_html(f)
+  cat(fs::path_file(f), ":",
+      html_text(html_nodes(h, "h1.mb-2")), # sometime empty (e.g. receptions)
+      str_trunc(html_text(html_nodes(h, "h1.mt-4")), 40), "\n")
+
+  # trivia: last 'session' is a golf event
+
+  a <- html_nodes(h, "a") %>%
+    html_attr("href") %>%
+    str_subset("submission/\\d+") %>%
+    str_replace(".*?#/", "https://virtual.oxfordabstracts.com/#/") %>%
+    unique()
+
+  if (!length(a))
+    next
+
+  # download abstracts, sometimes 0, sometimes 3, generally 4-5
+  for (j in a) {
+
+    f <- str_c("html/abstracts/abstract_", str_extract(j, "\\d+$"), ".html")
+    if (!fs::file_exists(f)) {
+
+      rd$navigate(j)
+      Sys.sleep(2)
+
+      rd$getPageSource()[[1]] %>%
+        readr::write_lines(f)
+
+    }
+
+    h <- read_html(f)
+    cat("  ", fs::path_file(f), ":",
+        # never empty, but varies without matching session id?
+        html_text(html_nodes(h, "h2.mb-1")),
+        str_trunc(html_text(html_nodes(h, "h1.my-5")), 40), "\n")
+
+  }
+
+}
+
+# kthxbye
---
+++ README.md
@@ -0,0 +1,7 @@
+Main project repository: [`briatte/epsaconf`](https://github.com/briatte/epsaconf)
+
+A graph of co-authorship ties between academic organizations, based on papers to be presented at the [European Political Science Association](https://www.epsanet.org/) (EPSA) 2023 conference.
+
+__Work in progress__ (i.e. no graph yet).
+
+See also Benjamin Guinaudeau's own [`epsa2023`](https://github.com/benjaminguinaudeau/epsa2023) repository, which contains a slightly different version of the same data.
